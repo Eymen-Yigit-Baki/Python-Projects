fa4cd3d44e4be0354b5b99a8af022dd1cbcb6f23
Eymen-Yigit-Baki <eymenbaki831@gmail.com>
2024-08-22 13:15:34 +0300

    Add files via upload

---
+++ Python=2024 YAZİLI ortalama hesaplayıcı.py
@@ -0,0 +1,16 @@
+#2024 Yazılı Ortalama Hesaplama (Ingilizce)
+
+
+#Not Bölümü::
+Speaking = int(input(("Bir Not Giriniz: ")))
+Listening = int(input(("Bir Not Giriniz: ")))
+Yazili = int(input(("Bir Not Giriniz: ")))
+
+#Formül Bölümü::
+S = Speaking/4
+L = Listening/4
+Y = Yazili/2
+
+#Ortalama Bölümü (Ana kod)::
+Ortalama_Not = (S+L+Y)
+print(Ortalama_Not)
